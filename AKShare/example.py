7d66f085188a227fdd2ff7bf017484378f43c827
wobushiludahai <wobushiludahai@163.com>
2024-04-07 00:32:30 +0800

    update

--- AKShare/example.py
+++ AKShare/example.py
@@ -1,4 +1,7 @@
 import akshare as ak
+import datetime
+import time
+import pandas as pd
 
-stock_sse_summary_df = ak.stock_sse_summary()
-print(stock_sse_summary_df)
+stock_hot_rank_wc_df = ak.stock_hot_rank_wc(date="20240403")
+print(stock_hot_rank_wc_df[['股票简称', '现价','涨跌幅','排名日期']].head(10))
