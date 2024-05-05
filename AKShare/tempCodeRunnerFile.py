73fe81d1f18dff46a50b1e6068cca9f2f4b49130
wobushiludahai <wobushiludahai@163.com>
2024-05-05 23:07:18 +0800

    update

--- AKShare/tempCodeRunnerFile.py
+++ AKShare/tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
-df = df.set_index('季度')
-# df.index = pd.to_datetime(df.index)
-# gdp = df['国内生产总值-绝对值'][::-1].astype('float')
+import akshare as ak
 
-# # GDP DIFF
-# gdp_diff = gdp.diff(4).dropna()
+stock_sse_summary_df = ak.stock_sse_summary()
+print(stock_sse_summary_df)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # somelink
 一些简单的link
+### 解决externally-managed-environment的问题
+```
+sudo apt install python3-venv
+python3 -m venv myenv
+source myenv/bin/activate
+pip install package-name
+```
