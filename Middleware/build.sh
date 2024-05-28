fbcd1f5cc4cd0b90bbd08307e0403ab47e23c9c3
wobushiludahai <wobushiludahai@163.com>
2024-05-28 23:33:51 +0800

    update

--- Middleware/build.sh
+++ Middleware/build.sh
@@ -33,7 +33,7 @@ SQLITE_INC=${TEST_INCLUDE_DIR}/sqlite
 mkdir -p ${SQLITE_INC}
 cp ${SQLITE_DIR}/build/*.h ${SQLITE_INC}
 cp -d ${SQLITE_DIR}/build/.libs/libsqlite3.so* ${RUN_DIR}
-rm -rf ${SQLITE_DIR}/build
+# rm -rf ${SQLITE_DIR}/build
 
 cd ${CJSON_DIR}
 git reset --hard
--- Middleware/tests/main.c
+++ Middleware/tests/main.c
@@ -1,106 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sqlite3.h"
+#include "cJSON.h"
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-   int i;
-   for(i=0; i<argc; i++){
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
+size_t ret;
 
 int main(void)
 {
-   sqlite3 *db;
-   char *zErrMsg = 0;
-   int rc;
-   char *sql;
+    // 打开JSON文件
+    FILE *file = fopen("/home/hai/github/someSample/Middleware/tests/property_t.json", "r");
+    if (!file) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
 
-   rc = sqlite3_open("test.db", &db);
+    // 读取文件内容
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char *data = (char *)malloc(length + 1);
+    ret = fread(data, 1, length, file);
+    fclose(file);
+    data[length] = '\0'; // 确保字符串以null结尾
 
-   if( rc ){
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      exit(0);
-   }else{
-      fprintf(stderr, "Opened database successfully\n");
-   }
+    // 解析JSON字符串
+    cJSON *json = cJSON_Parse(data);
+    if (!json) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL) {
+            fprintf(stderr, "Error before: [%s]\n", error_ptr);
+        }
+        free(data);
+        return EXIT_FAILURE;
+    }
 
-   /* Create SQL statement */
-   sql = "CREATE TABLE COMPANY("  \
-         "ID INT PRIMARY KEY     NOT NULL," \
-         "NAME           TEXT    NOT NULL," \
-         "AGE            INT     NOT NULL," \
-         "ADDRESS        CHAR(50)," \
-         "SALARY         REAL );";
+    // 打印解析结果
+    char *printed = cJSON_Print(json);
+    printf("%s\n", printed);
+    free(printed); // 释放cJSON_Print返回的字符串
 
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-   if( rc != SQLITE_OK ){
-   fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   }else{
-      fprintf(stdout, "Table created successfully\n");
-   }
+    // 访问JSON中的值
+    cJSON *name = cJSON_GetObjectItemCaseSensitive(json, "name");
+    if (cJSON_IsString(name) && name->valuestring != NULL) {
+        printf("Name: %s\n", name->valuestring);
+    }
 
-   /* Create SQL statement */
-   sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-         "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+    cJSON *age = cJSON_GetObjectItemCaseSensitive(json, "age");
+    if (cJSON_IsNumber(age)) {
+        printf("Age: %d\n", age->valueint);
+    }
 
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-   if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   }else{
-      fprintf(stdout, "Records created successfully\n");
-   }
-
-   /* Create SQL statement */
-   sql = "SELECT * from COMPANY";
-   const char* data = "Callback function called";
-
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
-   if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   }else{
-      fprintf(stdout, "Operation done successfully\n");
-   }
-
-   /* Create merged SQL statement */
-   sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1; " \
-         "SELECT * from COMPANY";
-
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
-   if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   }else{
-      fprintf(stdout, "Operation done successfully\n");
-   }
-
-   /* Create merged SQL statement */
-   sql = "DELETE from COMPANY where ID=2; " \
-         "SELECT * from COMPANY";
-
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
-   if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   }else{
-      fprintf(stdout, "Operation done successfully\n");
-   }
-
-   sqlite3_close(db);
+    // 清理
+    cJSON_Delete(json);
+    free(data);
 }
