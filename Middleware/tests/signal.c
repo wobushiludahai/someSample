ffe09818663b99e697a1b2cb272d08ba9aef59a6
landsea <wobushiludahai@163.com>
2024-07-23 00:05:21 +0800

    add signal

--- Middleware/tests/signal.c
+++ Middleware/tests/signal.c
@@ -2,24 +2,17 @@
 #include "gio/gio.h"
 #include "dbus/dbus.h"
 
-// dbus_message_new_signal
-
 void send_signal_by_gdbus(void)
 {
-    // g_dbus_connection_emit_signal
+    GDBusConnection *c;
+    c = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+
+    g_dbus_connection_emit_signal(c, "org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus",
+        "NameOwnerChanged", g_variant_new("(sss)", "test", "old", "new"), NULL);
+    g_object_unref(c);
 }
 
 void send_signal_by_dbus(void)
-{
-    // dbus_message_new_signal
-}
-
-void subscribe_signal_by_gdbus(void)
-{
-    // g_dbus_connection_signal_subscribe
-}
-
-void subscribe_signal_by_dbus(void)
 {
     DBusConnection *conn = NULL;
     DBusMessage *signal = NULL;
@@ -32,18 +25,6 @@ void subscribe_signal_by_dbus(void)
     }
 
     signal = dbus_message_new_signal("/org/freedesktop/DBus", "org.freedesktop.DBus", "NameOwnerChanged");
-
-    // dbus_message_set_sender(signal, "org.freedesktop.DBus");
-
-    // dbus_message_set_destination(signal, "org.freedesktop.DBus");
-
-    // dbus_message_set_path(signal, "/org/freedesktop/DBus");
-
-    // dbus_message_set_interface(signal, "org.freedesktop.DBus");
-
-    // dbus_message_set_signature(signal, "sss");
-
-    g_print("Send signal\n");
 
     char *server = "test";
     char *old = "old";
@@ -67,8 +48,38 @@ void subscribe_signal_by_dbus(void)
     dbus_connection_unref(conn);
 }
 
+// 假设的信号处理函数
+static void on_signal_received(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path,
+    const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data)
+{
+    g_print("Received signal: %s\n", signal_name);
+    // 在这里处理信号
+}
+
+GDBusConnection *c_test = NULL;
+void subscribe_signal_by_gdbus(void)
+{
+    c_test = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+
+    g_dbus_connection_signal_subscribe(c_test, "org.freedesktop.DBus", "org.freedesktop.DBus", "NameLost",
+        "/org/freedesktop/DBus", NULL, G_DBUS_SIGNAL_FLAGS_NONE, on_signal_received, NULL, NULL);
+
+    g_print("Subscribe signal success\n");
+}
+
+void subscribe_signal_by_dbus(void)
+{
+    // dbus_bus_add_match
+}
+
 void test_signal(void)
 {
+    subscribe_signal_by_gdbus();
+    g_usleep(2000 * 1000);
     g_print("%s\n", __func__);
-    subscribe_signal_by_dbus();
+    send_signal_by_dbus();
+
+    g_usleep(2000 * 1000);
+
+    send_signal_by_gdbus();
 }
